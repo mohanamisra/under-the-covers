755dca6361896a2f4d1cdeda79239d21012ae7ef
Mohana Misra <iammohanamisra@gmail.com>
2023-12-23 12:56:38 +0530

    Application working

--- models/book.js
+++ models/book.js
@@ -1,12 +1,4 @@
 const mongoose = require('mongoose')
-
-if (process.argv.length<3) {
-    console.log('give password as argument')
-    process.exit(1)
-}
-
-const password = process.argv[2]
-
 const url = process.env.MONGODB_URI;
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
